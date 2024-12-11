e553aff3b5ead72f451c45546819743b8adb614b
bobekov <stefan.bobekov@abv.bg>
2024-12-11 23:57:28 +0200

    add code

--- travel_project/settings.py
+++ travel_project/settings.py
@@ -28,11 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', config('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
 
+DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
 
 # Application definition
 
